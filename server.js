ce4136d23b65592fb469810cd38f475f94b46e78
Dibou960 <126475676+Dibou960@users.noreply.github.com>
2024-03-21 22:43:45 +0000

    server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ db.on('error', console.error.bind(console, 'Connection error:'));
 db.once('open', () => {
     console.log('Connected to MongoDB');
 });
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(cors({
